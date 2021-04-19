26fd51e266ce42fe23671ef84d7e994f9171e52f
Alex Vaslow <abv10@case.edu>
2021-04-19 16:45:47 -0400

    Added Oliver's changes from Google Drive

---
+++ Assets/Scripts/RandomDirectionEnemy.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomDirectionEnemy : MonoBehaviour
+{
+    public float speed = 2.0f;
+    private GameObject player;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    private float movementTime;
+
+    public Transform enemyThrowPoint;
+    public GameObject ballPrefab;
+    public float force = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        player = GameObject.Find("Player");
+        movement = new Vector2(Random.Range(-100f, 100f), Random.Range(-100f, 100f));
+        movement.Normalize();
+        movementTime = Random.Range(1.0f, 3.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.transform.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90f;
+        rb.rotation = angle;
+        if (movementTime <= 0)
+        {
+            movement = new Vector2(Random.Range(-100f, 100f), Random.Range(-100f, 100f));
+            movement.Normalize();
+            movementTime = Random.Range(1.0f, 3.0f);
+            Throw();
+        }
+        movementTime -= Time.deltaTime;
+
+    }
+
+    private void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * speed * Time.deltaTime));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        movement.x = -movement.x;
+        movement.y = -movement.y;
+    }
+
+    public void Throw()
+    {
+        GameObject ball = Instantiate(ballPrefab, enemyThrowPoint.position, enemyThrowPoint.rotation);
+        Rigidbody2D rb = ball.GetComponent<Rigidbody2D>();
+        rb.AddForce(enemyThrowPoint.up * force, ForceMode2D.Impulse);
+    }
+
+}
--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -4,9 +4,26 @@ using System.Collections;
 public class SceneController : MonoBehaviour
 {
 	[SerializeField] private GameObject enemyPrefab;
+	private GameObject player;
 	private GameObject enemy;
 	public static int enemyCount = 0;
 	private int wave = 0;
+	private Vector2[] locations;
+
+	void Start()
+	{
+		locations = new Vector2[10];
+		locations[0] = new Vector2(0f, 4.2f);
+		locations[1] = new Vector2(0f, -4.2f);
+		locations[2] = new Vector2(5.8f, 1.6f);
+		locations[3] = new Vector2(-5.8f, -1.6f);
+		locations[4] = new Vector2(5.8f, -1.6f);
+		locations[5] = new Vector2(-5.8f, 1.6f);
+		locations[6] = new Vector2(-2.5f, 4.2f);
+		locations[7] = new Vector2(2.5f, -4.2f);
+		locations[8] = new Vector2(2.5f, 4.2f);
+		locations[9] = new Vector2(-2.5f, -4.2f);
+	}
 
 	void Update()
 	{
@@ -32,13 +49,8 @@ public class SceneController : MonoBehaviour
 			for (int i = wave; i > 0; i--)
 			{
 				enemy = Instantiate(enemyPrefab) as GameObject;
-				Vector2 location = new Vector2(Random.Range(-5.8f, 5.8f), Random.Range(-4.2f, 4.2f));
+				Vector2 location = locations[i - 1];
 				enemy.transform.position = location;
-				while (enemy.GetComponent<Collider>() != null)
-				{
-					location = new Vector2(Random.Range(-5.8f, 5.8f), Random.Range(-4.2f, 4.2f));
-					enemy.transform.position = location;
-				}
 				float angle = Random.Range(0, 360);
 				enemy.transform.Rotate(0, 0, angle);
 				enemyCount++;
@@ -49,13 +61,8 @@ public class SceneController : MonoBehaviour
 			for (int i = 10; i > 0; i--)
 			{
 				enemy = Instantiate(enemyPrefab) as GameObject;
-				Vector2 location = new Vector2(Random.Range(-5.8f, 5.8f), Random.Range(-4.2f, 4.2f));
+				Vector2 location = locations[i - 1];
 				enemy.transform.position = location;
-				while (enemy.GetComponent<Collider>() != null)
-				{
-					location = new Vector2(Random.Range(-5.8f, 5.8f), Random.Range(-4.2f, 4.2f));
-					enemy.transform.position = location;
-				}
 				float angle = Random.Range(0, 360);
 				enemy.transform.Rotate(0, 0, angle);
 				enemyCount++;
@@ -64,5 +71,4 @@ public class SceneController : MonoBehaviour
 		enemyCount -= 1;
 
 	}
-
 }
