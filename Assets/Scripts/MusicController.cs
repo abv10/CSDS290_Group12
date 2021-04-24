a03c1e37b4299e0ea410f1be2cf56df9cff83012
Alex Vaslow <abv10@case.edu>
2021-04-24 17:17:19 -0400

    Music Controller Added

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MusicController : MonoBehaviour
+{
+    [SerializeField] private Button soundOn;
+    [SerializeField] private Button soundOff;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundOn.onClick.AddListener(delegate { TurnMusicOff(); });
+        soundOff.onClick.AddListener(delegate { TurnMusicOn(); });
+        PlayerPrefs.SetInt("music", 1);
+        PlayerPrefs.Save();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TurnMusicOn()
+    {
+        soundOff.gameObject.SetActive(false);
+        soundOn.gameObject.SetActive(true);
+        PlayerPrefs.SetInt("music", 1);
+        PlayerPrefs.Save();
+    }
+
+    void TurnMusicOff()
+    {
+        soundOff.gameObject.SetActive(true);
+        soundOn.gameObject.SetActive(false);
+        PlayerPrefs.SetInt("music", 0);
+        PlayerPrefs.Save();
+    }
+}
