be2e1264fb485ea99796428dd28788a1b52e806d
abv10 <78226173+abv10@users.noreply.github.com>
2021-05-03 19:05:11 -0400

    Random enemy locations, shield changes

---
+++ PlayerCharacter.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+	private int _health;
+	private int _streak;
+	private bool lose;
+
+	void Start()
+	{
+		_health = 5;
+		_streak = 0;
+		lose = false;
+	}
+
+	public void Hurt()
+	{
+		var sceneController = FindObjectOfType<SceneController>();
+		if (sceneController.getShieldActive() == false) {
+			_health -= 1;
+			sceneController.StartCoroutine(sceneController.ShieldActivate(0.25f));
+		}
+	}
+
+	public void StreakIncrease()
+    {
+		_streak++;
+    }
+
+	public void StreakReset()
+    {
+		_streak = 0;
+    }
+
+	void Update()
+	{
+		if (_health <= 0 && lose == false)
+        {
+			lose = true;
+			FindObjectOfType<SceneController>().Lose();
+		}
+	}
+
+	public int getHealth()
+	{
+		return _health;
+	}
+
+	public int getStreak()
+	{
+		return _streak;
+	}
+
+	public void HealthPowerUp()
+    {
+		_health += 1;
+    }
+}
--- SceneController.cs
+++ SceneController.cs
@@ -2,32 +2,56 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using System.Collections.Generic;
 
 public class SceneController : MonoBehaviour
 {
-	[SerializeField] private GameObject enemyPrefab;
+	[SerializeField] private GameObject randomEnemy;
+	[SerializeField] private GameObject followerEnemy;
+	[SerializeField] private GameObject shieldEnemy;
 	private GameObject player;
 	private GameObject enemy;
 	public static int enemyCount = 0;
 	private int wave = 0;
 	private Vector2[] locations;
+
+	private int speedToAdd = 0;
+	private int forceToAdd = 0;
+
+	[SerializeField] private GameObject healthPowerUpPrefab;
+	private GameObject healthPowerUp;
+	private bool healthspawn;
+
+	[SerializeField] private GameObject shieldPrefab;
+	private GameObject shield;
+	private bool shieldspawn;
+	private bool shieldactive;
+	private float shieldTimeRemaining = 0;
+
+	private bool explosiveballactive;
+
 	[SerializeField] private Text waveCount;
+	[SerializeField] private Text healthCount;
+	[SerializeField] private Text highScore;
+	[SerializeField] private Text countdown;
+	[SerializeField] private Text streakCount;
+	[SerializeField] private Text shieldtext;
+	[SerializeField] private Text explosiveballtext;
+	private int highScoreValue;
 	public Button quit;
 	public Button restart;
+	public GameObject gameover;
+	[SerializeField] private Text gameovertext;
+	[SerializeField] private Text finalwave;
+	[SerializeField] private Text higheststreak;
+	private int streakhighscore;
 
-    public void Quit()
-    {
-        Application.Quit();
-    }
-
-    public void UpdateScore()
-    {
-        waveCount.text = "Wave: " + wave;
-    }
-
-    void Start()
+	void Start()
 	{
+		StartCoroutine(CountDown());
+		gameover.SetActive(false);
+		wave = 0;
+		enemyCount = 0;
+		streakhighscore = 0;
 		locations = new Vector2[10];
 		locations[0] = new Vector2(0f, 4.2f);
 		locations[1] = new Vector2(0f, -4.2f);
@@ -40,54 +64,164 @@ public class SceneController : MonoBehaviour
 		locations[8] = new Vector2(2.5f, 4.2f);
 		locations[9] = new Vector2(-2.5f, -4.2f);
 
-		quit.onClick.AddListener(delegate { Quit(); });
-		restart.onClick.AddListener(delegate { SceneManager.LoadScene("Game"); });
+		healthspawn = false;
 
+		shieldtext.color = new Color32(217, 6, 6, 255);
+		explosiveballtext.color = new Color32(217, 6, 6, 255);
+		shieldspawn = true;
+		shieldactive = false;
+
+		explosiveballactive = false;
+
+		quit.onClick.AddListener(delegate { SceneManager.LoadScene("StartScreen"); });
+		restart.onClick.AddListener(delegate { SceneManager.LoadScene("Game"); });
 	}
 
 	void Update()
 	{
-		if (enemyCount == 0)
+		UpdateHealth();
+		UpdateStreak();
+
+		
+
+		//1 Health Power Up spawns every 3 waves
+		if (wave != 0 && wave % 3 == 0 && healthspawn == true)
 		{
-			enemyCount = 1;
-			StartCoroutine(NewWave());
-			UpdateScore();
+			healthPowerUp = Instantiate(healthPowerUpPrefab) as GameObject;
+			healthPowerUp.transform.position = new Vector2(0,0);
+			healthspawn = false;
 		}
 
+		//Streak Power Ups
+		if (FindObjectOfType<PlayerCharacter>().getStreak() == 0)
+        {
+			if (shieldactive == false)
+            {
+				shieldspawn = true;
+				shieldtext.color = new Color32(217, 6, 6, 255);
+			}
+			explosiveballactive = false;
+			explosiveballtext.color = new Color32(217, 6, 6, 255);
+		}
+		if(shieldTimeRemaining > 0)
+        {
+			shieldTimeRemaining -= Time.deltaTime;
+		}
+		else
+        {
+			shieldTimeRemaining = 0f;
+        }
+		if (FindObjectOfType<PlayerCharacter>().getStreak() == 3 && shieldspawn == true)
+        {
+			StartCoroutine(ShieldActivate(3.0f));			
+		}
+
+		if (FindObjectOfType<PlayerCharacter>().getStreak() == 5 && explosiveballactive == false)
+        {
+			explosiveballactive = true;
+			FindObjectOfType<PlayerThrowing>().setExplosive(true);
+			explosiveballtext.color = new Color32(0, 200, 3, 255);
+		}
+		//New Wave
+		if (enemyCount == 0)
+		{
+			StartCoroutine(CountDown());
+			enemyCount = 1;
+			StartCoroutine(NewWave(shieldTimeRemaining));
+			healthspawn = true;
+		}
 	}
 
 	public void DecreaseEnemyCount()
-    {
+	{
 		enemyCount--;
-    }
+	}
 
-	public IEnumerator NewWave()
+	public IEnumerator NewWave(float shieldReactivateTime)
 	{
 		wave++;
-		Dodgeball.waveOver = true;
 
 		yield return new WaitForSeconds(5.0f);
+		UpdateScore();
+		var dodgeballs = FindObjectsOfType<Dodgeball>();
+		foreach (Dodgeball d in dodgeballs)
+		{
+			Destroy(d.gameObject);
+		}
+		//Reactivate Shield
+		if(shieldReactivateTime != 0)
+        {
+			StartCoroutine(ShieldActivate(shieldReactivateTime));
+		}
 
 		Vector2 player = GameObject.Find("Player").transform.position;
-
+		var availableLocations = new ArrayList() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 		if (wave <= 10)
 		{
 			for (int i = wave; i > 0; i--)
 			{
-				enemy = Instantiate(enemyPrefab) as GameObject;
-				Vector2 location = locations[i - 1];
+				int randomizer = UnityEngine.Random.Range(1, 10);
+				if (randomizer < 6)
+				{
+					enemy = Instantiate(followerEnemy) as GameObject;
+					enemy.GetComponent<Enemy>().increaseSpeed(speedToAdd);
+					enemy.GetComponent<Enemy>().increaseForce(forceToAdd);
+				}
+				else if (randomizer < 9) {
+					enemy = Instantiate(randomEnemy) as GameObject;
+					enemy.GetComponent<RandomDirectionEnemy>().increaseSpeed(speedToAdd);
+					enemy.GetComponent<RandomDirectionEnemy>().increaseForce(forceToAdd);
+				}
+				else
+				{
+					enemy = Instantiate(shieldEnemy) as GameObject;
+					enemy.GetComponent<ShieldEnemy>().increaseSpeed(speedToAdd);
+					enemy.GetComponent<ShieldEnemy>().increaseForce(forceToAdd);
+				}
+
+				//Prevents the player from predicting where the enemies will spawn
+				int locationNumber = Random.Range(0, availableLocations.Count);
+				var locIndex = availableLocations[locationNumber];
+				availableLocations.Remove(locIndex);
+				Vector2 location = locations[(int)locIndex];
+
 				enemy.transform.position = location;
-				float angle = Random.Range(0, 360);
+				float angle = UnityEngine.Random.Range(0, 360);
 				enemy.transform.Rotate(0, 0, angle);
 				enemyCount++;
 			}
+			speedToAdd++;
+			forceToAdd++;
 		}
 		else
 		{
+			int randomizer = Random.Range(1, 10);
 			for (int i = 10; i > 0; i--)
 			{
-				enemy = Instantiate(enemyPrefab) as GameObject;
-				Vector2 location = locations[i - 1];
+				if (randomizer < 6)
+				{
+					enemy = Instantiate(followerEnemy) as GameObject;
+					enemy.GetComponent<Enemy>().increaseSpeed(speedToAdd);
+					enemy.GetComponent<Enemy>().increaseForce(forceToAdd);
+				}
+				else if (randomizer < 9)
+				{
+					enemy = Instantiate(randomEnemy) as GameObject;
+					enemy.GetComponent<RandomDirectionEnemy>().increaseSpeed(speedToAdd);
+					enemy.GetComponent<RandomDirectionEnemy>().increaseForce(forceToAdd);
+				}
+				else
+				{
+					enemy = Instantiate(shieldEnemy) as GameObject;
+					enemy.GetComponent<ShieldEnemy>().increaseSpeed(speedToAdd);
+					enemy.GetComponent<ShieldEnemy>().increaseForce(forceToAdd);
+				}
+				//Prevents the player from predicting where the enemies will spawn
+				int locationNumber = Random.Range(0, availableLocations.Count);
+				var locIndex = availableLocations[locationNumber];
+				availableLocations.Remove(locIndex);
+				Vector2 location = locations[(int)locIndex];
+
 				enemy.transform.position = location;
 				float angle = Random.Range(0, 360);
 				enemy.transform.Rotate(0, 0, angle);
@@ -97,4 +231,115 @@ public class SceneController : MonoBehaviour
 		enemyCount -= 1;
 
 	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+
+	public void Lose()
+	{
+		if (wave > PlayerPrefs.GetInt("highscore"))
+		{
+			PlayerPrefs.SetInt("highscore", wave);
+			PlayerPrefs.SetString("date", System.DateTime.Now.ToLongDateString());
+			PlayerPrefs.Save();
+		}
+		gameover.SetActive(true);
+		StartCoroutine(Gameover());
+		finalwave.text = "Score: " + wave;
+		higheststreak.text = "Highest Streak: " + streakhighscore;
+		DisplayHighScore();
+		FindObjectOfType<MusicControllerGame>().GameOver();
+	}
+
+	public void UpdateScore()
+	{
+		waveCount.text = "Wave: " + wave;
+	}
+
+	public void UpdateHealth()
+	{
+		healthCount.text = "Health: " + FindObjectOfType<PlayerCharacter>().getHealth();
+	}
+
+	public void UpdateStreak()
+    {
+		streakCount.text = "Streak: " + FindObjectOfType<PlayerCharacter>().getStreak();
+		if (streakhighscore < FindObjectOfType<PlayerCharacter>().getStreak())
+        {
+			streakhighscore = FindObjectOfType<PlayerCharacter>().getStreak();
+		}
+	}
+
+	public int getWave()
+    {
+		return wave;
+    }
+
+	public bool getShieldActive()
+    {
+		return shieldactive;
+    }
+
+	public IEnumerator ShieldActivate(float time)
+    {
+		shieldTimeRemaining = time;
+		shield = Instantiate(shieldPrefab) as GameObject;
+		shield.transform.position = FindObjectOfType<PlayerCharacter>().transform.position;
+		shieldspawn = false;
+		shieldactive = true;
+		shieldtext.color = new Color32(0, 200, 3, 255);
+
+		yield return new WaitForSeconds(time);
+		
+		shieldactive = false;
+		Destroy(shield);
+		if (FindObjectOfType<PlayerCharacter>().getStreak() < 3)
+        {
+			shieldspawn = true;
+			shieldtext.color = new Color32(217, 6, 6, 255);
+		}
+	}
+
+	public IEnumerator Gameover()
+    {
+		while(true)
+        {
+			gameovertext.text = "GAMEOVER";
+
+			yield return new WaitForSeconds(0.75f);
+
+			gameovertext.text = "GAMEOVER_";
+
+			yield return new WaitForSeconds(0.75f);
+		}
+	}
+
+	public void DisplayHighScore()
+	{
+		if (PlayerPrefs.GetInt("highscore") == 0)
+		{
+			PlayerPrefs.SetInt("highscore", 0);
+			PlayerPrefs.Save();
+		}
+		highScore.text = "High Score: " + PlayerPrefs.GetInt("highscore");
+	}
+
+	public IEnumerator CountDown()
+    {
+		countdown.gameObject.SetActive(true);
+		countdown.text = "Next Wave In:\n5";
+		yield return new WaitForSeconds(1.0f);
+		countdown.text = "Next Wave In:\n4";
+		yield return new WaitForSeconds(1.0f);
+		countdown.text = "Next Wave In:\n3";
+		yield return new WaitForSeconds(1.0f);
+		countdown.text = "Next Wave In:\n2";
+		yield return new WaitForSeconds(1.0f);
+		countdown.text = "Next Wave In:\n1";
+		yield return new WaitForSeconds(1.0f);
+		countdown.gameObject.SetActive(false);
+	}
+
 }
